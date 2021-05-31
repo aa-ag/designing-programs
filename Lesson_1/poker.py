e0822cfc3bac5c5d88164bd4eaca18557aa12bb9
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-31 13:48:55 -0500

    added boilerplate code for poker.py excersice. TO DO: need to write up hand_rank() helper function

--- Lesson_1/poker.py
+++ Lesson_1/poker.py
@@ -3,14 +3,25 @@
 ############------------ GLOBAL VARIABLES ------------############
 
 ############------------ FUNCTIONS ------------############
-def poker():
-    '''
-     returns best hand: poker([hands...]) => hand
-    '''
-    print(max([3, 4, 5, 0]), max([3, 4, -5, 0], key=abs))
-    # 5 -5
+# -----------
+# User Instructions
+#
+# Modify the poker() function to return the best hand
+# according to the hand_rank() function. Since we have
+# not yet written hand_rank(), clicking RUN won't do
+# anything, but clicking SUBMIT will let you know if you
+# have gotten the problem right.
+#
+
+def poker(hands):
+    "Return the best hand: poker([hand,...]) => hand"
+    return max(hands, key=hand_rank)
+
+
+def hand_rank(hand):
+    pass
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    poker()
+    poker([(5, 'S'), (6, 'S'), (7, 'S')])
